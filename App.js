74a4e83b918274569bdbb29ab6d2ab33e1af422d
KhanhDTP <khanhdtp@evolableasia.vn>
2020-11-19 17:23:12 +0700

    Build apk-dev-with-bundle...

--- App.js
+++ App.js
@@ -8,26 +8,21 @@
 
 import React, {PureComponent} from 'react';
 import {
-  SafeAreaView,
+  // SafeAreaView,
   StyleSheet,
   ScrollView,
   View,
   Text,
+  TextInput,
   StatusBar,
   PermissionsAndroid
 } from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-// import SmsRetriever from 'react-native-sms-retriever';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 import SmsListener from 'react-native-android-sms-listener';
 
+/**
+ * 
+ */
 export default class App extends PureComponent
 {
   /**
@@ -37,23 +32,40 @@ export default class App extends PureComponent
   constructor(props) {
     super(props);
 
+    // Init state
     this.state = {
+      /** @var {Array} */
       smsArr: []
     };
-  }
 
-  componentDidMount() {
-    this._requestReadSmsPermission();
     //
-    SmsListener.addListener(sms => {
+    this._smsListener = SmsListener.addListener(async _sms => {
+      // Parse sms content
+      let sms = this._parseSms(_sms);
+
+      // Send data to API
+      sms = await this._sendAPI(sms);
+
+      // Change state, trigger re-render
       this.setState(({smsArr}) =>  {
-        smsArr = smsArr.concat(sms);
+        smsArr = [sms].concat(smsArr);
         return {smsArr};
       });
     });
   }
 
-  _requestReadSmsPermission = async () => {
+  /** @var {Object} */
+  _smsListener = null;
+
+  componentDidMount = async () => {
+    await this._requestPermissions();
+  };
+
+  componentWillUnmount = () => {
+    this._smsListener.remove();
+  };
+
+  _requestPermissions = async () => {
     await PermissionsAndroid.request(
       PermissionsAndroid.PERMISSIONS.READ_SMS,
       {
@@ -68,59 +80,155 @@ export default class App extends PureComponent
         message: "plan2travel"
       }
     );
-  }
+  };
+
+  /** @var {String} */
+  _apiUrl = 'https://plan2travel.com.vn/api/sms2order';
+
+  /**
+   * @param {*} text 
+   */
+  handleChangeApiUrl = (text) => {
+    this._apiUrl = text;
+  };
+
+  /**
+   * Parse sms content
+   * @param {Object<
+   *  originatingAddress: string,
+   *  body: string,
+   *  timestamp: number
+   * >} _sms
+   * @return {Object<
+   *  originatingAddress: string,
+   *  body: string,
+   *  timestamp: number,
+   *  ...
+   * >}
+   */
+  _parseSms = (_sms) => {
+    let pattern = /SD TK ?(\d+) ?([+-])?([\d,.]+)VND ?luc ?(\d{2}-\d{2}-\d{4} \d{2}:\d{2}:\d{2})\. ?SD ?[+-]?([\d,.]+)VND\. ?Ref ?([\d,.]+)\.(.*)/i;
+    let m = new String(_sms.body).trim().match(pattern) || [];
+    //
+    // +++
+    let sms = Object.assign({}, {
+      'ID': '',
+      'so_tien': ((m[2] || '') == '-' ? -1 : 1) * (m[3] || '').replace(/[,.]/g, ''),
+      'thoi_gian': m[4] || '',
+      'noi_dung': m[7] || '',
+      'so_tk': m[1] || '',
+      'so_du': 1 * (m[5] || '').replace(/[,.]/g, ''),
+      '_api': '',
+    }, _sms);
+    // +++
+    if (sms.noi_dung) {
+      sms.ID = (m = sms.noi_dung.match(pattern = /PT[TVH][\d]+/i) || [])[0] || '';
+      if (sms.ID) {
+        sms.noi_dung = sms.noi_dung.replace(sms.ID, '');
+      }
+    }
+    // +++
+    return sms;
+  };
+  
+  /**
+   * 
+   * @param {*} sms 
+   */
+  _sendAPI = async (sms) => {
+    let _api = '0';
+    // Validate
+    if (sms && sms.ID) {
+      _api = '1';
+      try {
+        let resp = await fetch(this._apiUrl, {
+          method: 'POST',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(sms)
+        });
+      } catch (error) {
+        _api = error.message;
+      }
+    }
+    //
+    return Object.assign(sms, {_api});
+  };
 
   render() {
     let {smsArr} = this.state;
     return (
       <>
-        <StatusBar barStyle="dark-content" />
-        <View style={{flex: 1}}>
-          <Text>sms(s): {smsArr.length}</Text>
-          {smsArr.map((sms, idx) => {
-            return <Text key={`sms-${idx}`}>{sms.body}</Text>;
-          })}
+        <StatusBar barStyle="default" />
+        <View style={[styles.apiBar]}>
+          <TextInput
+            style={[styles.apiBarTxt]}
+            onChangeText={this.handleChangeApiUrl}
+            defaultValue={this._apiUrl}
+          />
         </View>
+        <View style={[styles.smsBar]}>
+          <Text><Text style={[styles.highlight]}>sms(s):</Text> {smsArr.length}.</Text>
+        </View>
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={[styles.scrollView]}
+        >
+          {smsArr.map((sms, idx) => {
+            // Limit?
+            if (idx >= 100) {
+              return null;
+            }
+            return <View key={`sms-${idx}`} style={[styles.sms]}>
+              <Text
+                style={[sms.ID ? null : styles.smsTxtNG]}
+                selectable={true}
+              >
+                <Text style={[styles.highlight]}>{1 + idx}.</Text> {JSON.stringify(sms, null, 2)}
+              </Text>
+            </View>;
+          })}
+        </ScrollView>
       </>
     );
   }
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  apiBar: {
+    padding: 1
+  },
+  apiBarTxt: {
+    height: 40,
+    borderWidth: 1,
+    borderColor: 'silver'
+  },
   scrollView: {
     backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
+    borderTopWidth: 1,
+    borderTopColor: 'gray'
   },
   body: {
     backgroundColor: Colors.white,
   },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  smsBar: {
+    padding: 12
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
+  sms: {
+    margin: 3,
+    padding: 6,
+    borderWidth: 1,
+    borderColor: 'gray'
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
+  smsTxtNG: {
+    color: 'red'
   },
   highlight: {
     fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
   },
 });
